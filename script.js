7da558e8bbe36efe18f20cce890d11b4c80f6edb
satoshi-13 <ce30.4925@gmail.com>
2026-09-15 10:12:10 +0900

    Update GAS_URL to the correct web app URL

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 // ================================================================
 
 // ▼▼▼ 必ず現在のGAS WebアプリURLに置き換えてください ▼▼▼
-const GAS_URL = 'PASTE_YOUR_GAS_WEB_APP_EXEC_URL_HERE';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbysonwNDi4hHt_KpzfCW1fZFBIeSwUeHa3u_2RjlAxnN70jp63SMlEs6m7Nzy81QI19/exec';
 // ▲▲▲ 例: https://script.google.com/macros/s/xxxxx/exec ▲▲▲
 
 const CACHE_TTL_MS = 60 * 1000;
